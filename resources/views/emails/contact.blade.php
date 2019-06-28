fddfea6c473396add8a8d9583980759eb5cfe36e
Boris <jaris.ultio.21@gmail.com>
2019-06-28 11:59:27 +0100

    Update 1 - Handling footer contact form

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,37 +1,42 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<style>
-    #logo {
-        width: 100px;
-    }
-</style>
-<div class="container">
-    <div class="row">
-        <div class="col-12">
-            <div class="d-flex justify-content-start py-3">
-                <img src="https://autoecoleuniversite.com/images/LOGO AUTO ECOLE UNIVERSITE.png" id="logo" alt="Logo de l'auto-école">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="d-flex justify-content-start py-3">
+                    <img src="https://autoecoleuniversite.com/images/LOGO AUTO ECOLE UNIVERSITE.png" id="logo" style="width: 100px;" alt="Logo de l'auto-école">
+                </div>
             </div>
-        </div>
-        <div class="col-12 bg-primary py-3 text-white">
-            <h4 class="text-center">Contact</h4>
-            <h6 class="text-center">Un nouveau message de visiteur</h6>
-            <p>
-                Les informations sont les suivantes :
-            </p>
-            <ul class="list-unstyled">
-                <li><strong><u>Nom :</u></strong> {{ $name }}</li>
-                <li><strong><u>Adresse mail :</u></strong> {{ $email }}</li>
-                <li><strong><u>Message :</u></strong> {{ $message }}</li>
-            </ul>
-        </div>
-        <div class="text-light text-center bg-dark">
-            <div class="p-3">
+            <div class="col-12 bg-primary py-3 text-white">
+                <h4 class="text-center">Contact</h4>
+                <h6 class="text-center">Un nouveau message de visiteur</h6>
                 <p>
-                    <a href="https://autoecoleuniversite.com" class="text-light">Auto-école Université</a>
+                    Les informations sont les suivantes :
                 </p>
-                <p>
-                    Copyright &copy; 2019. Tous droits réservés.
-                </p>
+                <ul class="list-unstyled">
+                    <li><strong><u>Nom :</u></strong> {{ $name }}</li>
+                    <li><strong><u>Adresse mail :</u></strong> {{ $email }}</li>
+                    <li><strong><u>Message :</u></strong> {{ $message }}</li>
+                </ul>
+            </div>
+            <div class="text-light text-center bg-dark">
+                <div class="p-3">
+                    <p>
+                        <a href="https://autoecoleuniversite.com" class="text-light">Auto-école Université</a>
+                    </p>
+                    <p>
+                        Copyright &copy; 2019. Tous droits réservés.
+                    </p>
+                </div>
             </div>
         </div>
     </div>
-</div>
+</body>
+</html>
--- resources/views/emails/wecashup/shipped.blade.php
+++ resources/views/emails/wecashup/shipped.blade.php
@@ -1,35 +1,40 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<style>
-    #logo {
-        width: 100px;
-    }
-</style>
-<div class="container">
-    <div class="row">
-        <div class="col-12">
-            <div class="d-flex justify-content-start py-3">
-                <img src="https://autoecoleuniversite.com/images/LOGO AUTO ECOLE UNIVERSITE.png" id="logo" alt="Logo de l'auto-école">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="d-flex justify-content-start py-3">
+                    <img src="https://autoecoleuniversite.com/images/LOGO AUTO ECOLE UNIVERSITE.png" id="logo" style="width: 100px;" alt="Logo de l'auto-école">
+                </div>
             </div>
-        </div>
-        <div class="col-12 bg-primary text-white py-3">
-            <h4 class="text-center">Informations sur la transaction</h4>
-            <p>Les informations sont donc les suivantes :</p>
-            <ul class="list-unstyled">
-                <li><strong><u>Détails :</u></strong> {{ $transaction->transaction_details }}</li>
-                <li><strong><u>Montant :</u></strong> {{ $transaction->transaction_amount }} ({{ $transaction->transaction_receiver_currency }})</li>
-                <li><strong><u>Statut :</u></strong> {{ $transaction->transaction_status }}</li>
-                <li><strong><u>Type :</u></strong> {{ $transaction->transaction_type }}</li>
-            </ul>
-        </div>
-        <div class="text-light text-center bg-dark">
-            <div class="p-3">
-                <p>
-                    <a href="https://autoecoleuniversite.com" class="text-light">Auto-école Université</a>
-                </p>
-                <p>
-                    Copyright &copy; 2019. Tous droits réservés.
-                </p>
+            <div class="col-12 bg-primary text-white py-3">
+                <h4 class="text-center">Informations sur la transaction</h4>
+                <p>Les informations sont donc les suivantes :</p>
+                <ul class="list-unstyled">
+                    <li><strong><u>Détails :</u></strong> {{ $transaction->transaction_details }}</li>
+                    <li><strong><u>Montant :</u></strong> {{ $transaction->transaction_amount }} ({{ $transaction->transaction_receiver_currency }})</li>
+                    <li><strong><u>Statut :</u></strong> {{ $transaction->transaction_status }}</li>
+                    <li><strong><u>Type :</u></strong> {{ $transaction->transaction_type }}</li>
+                </ul>
+            </div>
+            <div class="text-light text-center bg-dark">
+                <div class="p-3">
+                    <p>
+                        <a href="https://autoecoleuniversite.com" class="text-light">Auto-école Université</a>
+                    </p>
+                    <p>
+                        Copyright &copy; 2019. Tous droits réservés.
+                    </p>
+                </div>
             </div>
         </div>
     </div>
-</div>
+</body>
+</html>
