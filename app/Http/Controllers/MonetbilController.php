99f29f033d0f3ac3f6698de5fb279e3a0cc78294
Boris <jaris.ultio.21@gmail.com>
2019-07-03 15:32:39 +0100

    Fixed Mail missing error

--- app/Http/Controllers/MonetbilController.php
+++ app/Http/Controllers/MonetbilController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Mail\TransactionShipped;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Mail;
 
 class MonetbilController extends Controller
 {
