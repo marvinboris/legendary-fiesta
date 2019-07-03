fbaa44d6d642d71b2e3d5d701f7f0c5a6db8cdd9
Boris <jaris.ultio.21@gmail.com>
2019-07-03 15:31:22 +0100

    Fixed Session missing error

--- app/Http/Controllers/MonetbilController.php
+++ app/Http/Controllers/MonetbilController.php
@@ -8,6 +8,7 @@ use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Mail\TransactionShipped;
+use Illuminate\Support\Facades\Session;
 
 class MonetbilController extends Controller
 {
