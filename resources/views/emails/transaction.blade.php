1c9095a3d2959114d2c547f3b8d4798f05467bfa
Boris <jaris.ultio.21@gmail.com>
2019-07-03 15:35:17 +0100

    Fixed missing element in transaction email blade file

--- resources/views/emails/transaction.blade.php
+++ resources/views/emails/transaction.blade.php
@@ -7,12 +7,12 @@
             La transaction a été effectuée avec succès. Veuillez cliquer sur le lien suivant pour retourner sur le site de votre auto-école :
             <br>
             <br>
-            <a href="{{ route('trainings.mine.show', $transaction->item_ref) }}">Retourner sur le site</a>
+            <a href="{{ route('trainings.mine.show', $transaction->training_id) }}">Retourner sur le site</a>
         @else
             La transaction n'a pas abouti. Veuillez cliquer sur le lien suivant pour réessayer :
             <br>
             <br>
-            <a href="{{ route('trainings.show', $transaction->item_ref) }}">Retourner sur le site</a>
+            <a href="{{ route('trainings.show', $transaction->training_id) }}">Retourner sur le site</a>
         @endif
         <br>
         <br>
