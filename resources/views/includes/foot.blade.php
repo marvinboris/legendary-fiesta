5f35e327f1754340ed2cab84545359acfdca797d
Boris <jaris.ultio.21@gmail.com>
2019-07-09 11:05:26 +0100

    Fixing loading block

--- resources/views/includes/foot.blade.php
+++ resources/views/includes/foot.blade.php
@@ -1,8 +1,8 @@
-    <div class="fixed-top loading bg-white w-100 h-100 d-flex justify-content-center align-items-center flex-column">
+    <div class="fixed-top loading bg-white w-100 h-100 d-flex justify-content-center align-items-center flex-column" style="z-index: 20000; ">
         <div>
-            <img src="{{ url('/') . '/images/LOGO AUTO ECOLE UNIVERSITE.png' }}" alt="Logo" class="logo">
+            <img src="{{ url('/') . '/images/LOGO AUTO ECOLE UNIVERSITE.png' }}" alt="Logo" class="logo" style="height: 50px;">
         </div>
-        <div class="spinner-border text-primary position-absolute" role="status"></div>
+        <div class="spinner-border text-primary position-absolute" role="status" style="width: 12rem; height: 12rem; z-index: -1; top: calc(50% - 6rem); left: calc(50% - 6rem);"></div>
     </div>
     <script>
         window.addEventListener('load', function () {
